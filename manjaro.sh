c45b71d6ece6a0027d228046d85cef2b8b070755
krmanik <12841290+krmanik@users.noreply.github.com>
2022-03-15 17:16:46 +0800

    fix: #16

--- manjaro.sh
+++ manjaro.sh
@@ -207,8 +207,8 @@ cat >>"${directory}/root/.bash_profile" <<- .
 ## Add new user account
 useradd -m -G wheel -s /bin/bash ${username}
 echo "${username}:${password}" | chpasswd
-sed -i -e "/root ALL=(ALL) ALL/a ${username} ALL=(ALL) ALL" \\
-  -e "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/" /etc/sudoers
+sed -i -e "/root ALL=(ALL:ALL) ALL/a ${username} ALL=(ALL:ALL) ALL" \\
+  -e "s/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/" /etc/sudoers
 echo "exec su - ${username}" >/root/.bash_profile
 bash /root/.bash_profile
 .
